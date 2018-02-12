5a2055a08da09b5f3a56075a3abe42440213e8bf
Bryant Eisenbach <fubuloubu@gmail.com>
2018-02-12 00:51:01 -0500

    Create Project.sol

---
+++ contracts/projects/Project.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.20;
+
+/*
+ * Project
+ *
+ * Project Manager owns project, which escrows funds for the client
+ * When a Module is added for the project, funds are sent to that contract for locking
+ */
+
+contract Project
